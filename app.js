560501405bab096844bd0333451b7905af38020e
Akshay <aksh.mahajan007@gmail.com>
2015-05-18 11:15:43 +0530

    Ready to deploy

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var app = require('express')();
+var ent = require('ent');
 var http= require('http').createServer(app);
 var io = require('socket.io')(http);
 
@@ -6,19 +7,21 @@ app.get('/', function(req,res){
   res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(8001,function(){
-console.log('listening at port : 8001');
+http.listen(3000,function(){
+console.log('listening at port : 3000');
 });
 
 io.sockets.on('connection', function (socket, username) {
 
   socket.on('add_user', function(username){
     console.log(username + ' is now connected');
+    username = ent.encode(username);
     socket.username = username; //store username
     socket.broadcast.emit('add_user', username);
   });
   
    socket.on('chat_msg', function(msg){
+   	msg = ent.encode(msg);
     console.log(socket.username + ' --> ' + msg);
     socket.broadcast.emit('chat_msg', {username: socket.username, msg: msg});
   });
